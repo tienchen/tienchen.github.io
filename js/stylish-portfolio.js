fe7add7a73881126730d983b7d004508c25ec12e
Thomas Chen <trc82@cornell.edu>
2020-09-21 17:54:14 -0400

    index and house pages

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -40,6 +40,27 @@
     }
   });
 
+  function showImages(el) {
+    var windowHeight = jQuery( window ).height();
+    $(el).each(function(){
+        var thisPos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (topOfWindow + windowHeight - 200 > thisPos ) {
+            $(this).addClass("fadein");
+        }
+    });
+  }
+
+  // if the image in the window of browser when the page is loaded, show that image
+  $(document).ready(function(){
+          showImages('.hidden');
+  });
+
+  // if the image in the window of browser when scrolling the page, show that image
+  $(window).scroll(function() {
+          showImages('.hidden');
+  });
 })(jQuery); // End of use strict
 
 // Disable Google Maps scrolling
